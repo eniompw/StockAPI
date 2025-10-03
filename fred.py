05c04538527218397081d0c4e372f1f9d631bccf
eniompw <71254710+eniompw@users.noreply.github.com>
2025-10-03 11:49:15 +0000

    single date

--- fred.py
+++ fred.py
@@ -1,6 +1,6 @@
 from fredapi import Fred
-fred = Fred(api_key='YOUR_API_KEY_HERE')
+fred = Fred(api_key='YOUR_API_KEY')
 
 target_date = '2023-11-01'
-yield_series = fred.get_series('DGS10', target_date, target_date)
+yield_series = fred.get_series('DGS10', target_date)
 print(yield_series.iloc[0])
