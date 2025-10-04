68775d126609c4bb60d5d2046d078673048b5c98
eniompw <71254710+eniompw@users.noreply.github.com>
2025-10-04 11:03:30 +0000

    start / end date example

--- fred.py
+++ fred.py
@@ -3,4 +3,6 @@ fred = Fred(api_key='YOUR_API_KEY')
 
 target_date = '2023-11-01'
 yield_series = fred.get_series('DGS10', target_date)
+# yield_series = fred.get_series('DGS10', start_date, end_date)
+
 print(yield_series.iloc[0])
